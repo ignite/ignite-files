c7684105c5e2ba807622af29d5ca722fab7b46d9
Pantani <Pantani>
2023-11-16 01:50:56 +0100

    bump hermes binary

--- hermes/data_linux_amd64.go
+++ hermes/data_linux_amd64.go
@@ -2,5 +2,5 @@ package hermes
 
 import _ "embed" // embed is required for binary embedding.
 
-//go:embed hermes-v1.6.0-x86_64-unknown-linux-gnu.tar.gz
+//go:embed hermes-v1.7.1-x86_64-unknown-linux-gnu.tar.gz
 var binaryCompressed []byte
