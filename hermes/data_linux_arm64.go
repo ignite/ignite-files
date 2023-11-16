f33ca4b91f79badc48faa94ec375b4d00eb74c24
Danilo Pantani <danpantani@gmail.com>
2023-11-16 16:17:53 +0100

    bump hermes binary (#17)
    Co-authored-by: Pantani <Pantani>

--- hermes/data_linux_arm64.go
+++ hermes/data_linux_arm64.go
@@ -2,5 +2,5 @@ package hermes
 
 import _ "embed" // embed is required for binary embedding.
 
-//go:embed hermes-v1.6.0-aarch64-unknown-linux-gnu.tar.gz
+//go:embed hermes-v1.7.1-aarch64-unknown-linux-gnu.tar.gz
 var binaryCompressed []byte
